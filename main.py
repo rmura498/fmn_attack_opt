5943188c185aa1eda2973bb62c0230febc554d15
Luca Scionis <lucascionis@tiscali.it>
2023-03-20 23:50:09 +0100

    Minor changes to main.py

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 import torch
 import torchvision
 
-from Utils.metrics import accuracy
 from Models.SmallCNN import SmallCNN
 from Models.downloadModel import download_model
 
+from Experiments.TestFMNAttack import TestFMNAttack
 from Attacks.FMN.FMNBase import FMNBase
 from Attacks.FMN.FMNOpt import FMNOpt
 
 
 if __name__=='__main__':
-    #TODO:  create a class to perform the experiments
-    #        - allows to perform a single experiment in a run (batch or single samples)
-    #        - allows to keep track of the experiment results (model accuracy, attack params)
-
-    # exp = TestAttack(model, dataset, batch (true, false), attack)
-    # exp.run()
-    # exp.plotEpsilonLoss() - plots epsilon and loss over the iteration
-
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model_params_path = download_model()
 
@@ -26,27 +17,23 @@ if __name__=='__main__':
     model.load_state_dict(torch.load(model_params_path, map_location=device))
     model.eval()
 
-    BATCH_SIZE = 10
-    mnist_test = torchvision.datasets.MNIST('./data', train=False, download=True,
-                                            transform=torchvision.transforms.ToTensor())
-    dl_test = torch.utils.data.DataLoader(mnist_test, batch_size=BATCH_SIZE, shuffle=False)
-
-    samples, labels = next(iter(dl_test))
-
-    acc = accuracy(model, samples, labels)
-    print("standard accuracy: ", acc)
-
-    x_adv = samples.clone()
-    norm = 0
+    batch_size = 10
+    norm = 1
     steps = 50
+    attack = FMNOpt
 
-    attack = FMNBase(model, x_adv, labels, norm=norm, steps=steps)
-    attack_opt = FMNOpt(model, x_adv, labels, norm=norm, steps=steps)
+    mnist_test = torchvision.datasets.MNIST('./data',
+                                            train=False,
+                                            download=True,
+                                            transform=torchvision.transforms.ToTensor())
 
-    advs = attack.run()
-    advs_opt = attack_opt.run()
+    exp = TestFMNAttack(model,
+                        mnist_test,
+                        attack=attack,
+                        steps=steps,
+                        norm=norm,
+                        batch_size=batch_size)
+    exp.run()
+    exp.plot()
 
-    acc = accuracy(model, advs, labels)
-    acc_opt = accuracy(model, advs_opt, labels)
-    print("Robust accuracy", acc)
-    print("Robust accuracy (opt)", acc_opt)
+
