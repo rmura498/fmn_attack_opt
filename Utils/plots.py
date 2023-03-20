6f9b13e8db899da6dec2663834ce376516574b90
Luca Scionis <lucascionis@tiscali.it>
2023-03-20 23:49:58 +0100

    Modified plots.py
    - changed normalization function for constant values
    - added model and attack in the plot title

--- Utils/plots.py
+++ Utils/plots.py
@@ -1,13 +1,26 @@
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
-import torch
+
+np.seterr(divide='ignore', invalid='ignore')
 
 
 def normalize_01(data):
-    return (data - np.min(data)) / (np.max(data) - np.min(data))
+    min_val = np.min(data)
+    max_val = np.max(data)
+
+    if min_val == max_val:
+        return np.divide(data, data)
+    else:
+        return (data - np.min(data)) / (np.max(data) - np.min(data))
 
 
-def plot_loss_epsilon_over_steps(loss, epsilon, steps, normalize=True):
+def plot_loss_epsilon_over_steps(loss,
+                                 epsilon,
+                                 steps,
+                                 attack_name='attack',
+                                 model_name='model',
+                                 normalize=True):
     if normalize:
         loss = normalize_01(loss)
         epsilon = normalize_01(epsilon)
@@ -26,6 +39,7 @@ def plot_loss_epsilon_over_steps(loss, epsilon, steps, normalize=True):
     ax1.grid()
     ax1.set_xlabel("Steps")
     ax1.set_ylabel("Loss/Epsilon")
+    ax1.title.set_text(f"Attack: {attack_name}, Model: {model_name}")
     fig1.legend()
 
     plt.show()
