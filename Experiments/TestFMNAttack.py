2f3f71edd04c0377f1160ed1766df6ccb38ffb28
GGiiuusseeppee <gf1997.gf@gmail.com>
2023-03-20 17:47:35 +0100

    Inizialization of TestFMNAttack class

--- Experiments/TestFMNAttack.py
+++ Experiments/TestFMNAttack.py
@@ -3,6 +3,7 @@ from Utils.metrics import accuracy
 from Utils.plots import plot_loss_epsilon_over_steps
 from torch.optim import SGD
 from torch.optim.lr_scheduler import CosineAnnealingLR
+import torch
 
 class TestFMNAttack(TestAttack):
     def __init__(self,
@@ -12,12 +13,12 @@ class TestFMNAttack(TestAttack):
                  batch_size=10,
                  optimizer=SGD,
                  scheduler=CosineAnnealingLR):
-        super().__init__(model, dataset, attack)
+        super().__init__(model, dataset, attack, batch_size, optimizer, scheduler)
+        self.dl_test = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
+        self.samples, self.labels = next(iter(self.dl_test))
 
-        #TODO: initialize samples and labels
 
-        # self.samples
-        # self.labels
+
 
     def run(self):
         # run the attack
@@ -25,7 +26,9 @@ class TestFMNAttack(TestAttack):
         pass
 
     def plot(self):
-        plot_loss_epsilon_over_steps(self.model.loss_per_iter, self.model.epsilon_per_iter, self.model.steps, normalize=False)
+        plot_loss_epsilon_over_steps(self.model.loss_per_iter,
+                                     self.model.epsilon_per_iter,
+                                     self.model.steps, normalize=False)
 
     def accuracy(self):
         return accuracy(self.model, self.samples, self.labels)
