a5023ba2a67a27897d69818b1cbcd28970c8f0b5
lucascionis <lucascionis@tiscali.it>
2023-04-18 17:38:06 +0200

    Fixed distribution and values bugs

--- Configs/search_spaces.py
+++ Configs/search_spaces.py
@@ -6,21 +6,27 @@ from optuna.distributions import\
     IntDistribution, \
     CategoricalDistribution,\
     LogUniformDistribution,\
-    UniformDistribution
+    UniformDistribution, \
+    DiscreteUniformDistribution
 
 
 OPTIMIZERS_SEARCH = {
     'SGD': {
-        'lr': LogUniformDistribution(0.5, 1),
+        'lr': FloatDistribution(0.5, 1, log=True),
         'momentum': FloatDistribution(0.81, 0.99),
-        'dampening': FloatDistribution(0, 0.2),
-        'Nesterov': CategoricalDistribution([False, True])
+        'dampening': FloatDistribution(0, 0.2)
+    },
+    'SGDNesterov': {
+        'lr': FloatDistribution(0.5, 1, log=True),
+        'momentum': FloatDistribution(0.81, 0.99),
+        'dampening': CategoricalDistribution([0]),
+        'nesterov': CategoricalDistribution([False, True])
     },
     'Adam':
     {
-        'lr': LogUniformDistribution(0.5, 1),
+        'lr': FloatDistribution(0.5, 1, log=True),
         'betas': FloatDistribution(0.81, 0.99),
-        'eps': LogUniformDistribution(1e-8, 1e-7),
+        'eps': FloatDistribution(1e-8, 1e-7, log=True),
         'amsgrad': CategoricalDistribution([False, True])
     }
 }
@@ -34,22 +40,18 @@ SCHEDULERS_SEARCH = {
         },
     'CosineAnnealingWarmRestarts':
         {
-            'T_0': lambda steps: steps/2,
-            'T_mult': 1,
-            'eta_min': 0,
-            'last_epoch': -1
+            'T_0': lambda steps: CategoricalDistribution([steps//2]),
+            'T_mult': CategoricalDistribution([1]),
+            'eta_min': CategoricalDistribution([0]),
+            'last_epoch': CategoricalDistribution([-1])
         },
     'MultiStepLR':
         {
-            'milestones': lambda steps: tune.sample_from(lambda steps: [
-                np.linspace(0, steps, 10),
-                np.linspace(0, steps, 5),
-                np.linspace(0, steps, 3)]),
-            'gamma': UniformDistribution(0.1, 0.9)
+            'gamma': FloatDistribution(0.1, 0.9)
         },
     'ReduceLROnPlateau':
         {
-            'factor': UniformDistribution(0.1, 0.5),
+            'factor': FloatDistribution(0.1, 0.5),
             'patience': CategoricalDistribution([5, 10, 20]),
             'threshold': LogUniformDistribution(1e-5, 1e-3)
         }
