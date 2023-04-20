9e4b6a42f82c7573c44687edc36646ef4a16d247
lucascionis <lucascionis@tiscali.it>
2023-04-20 22:36:24 +0200

    Testing tuning with PBT

---
+++ Configs/search_spaces_pbt.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+from ray import tune
+
+
+OPTIMIZERS_SEARCH_PBT = {
+    'SGD': {
+        'lr': tune.loguniform(1, 100),
+        'momentum': tune.uniform(0.81, 0.99),
+        'weight_decay': tune.loguniform(0.01, 10),
+        'dampening': tune.uniform(0, 0.2)
+    },
+    'SGDNesterov': {
+        'lr': tune.loguniform(1, 100),
+        'momentum': tune.uniform(0.81, 0.99),
+        'dampening': 0,
+        'nesterov': True
+    },
+    'Adam':
+    {
+        'lr': tune.loguniform(1, 100),
+        'weight_decay': tune.loguniform(0.01, 10),
+        'eps': 1e-8,
+        'amsgrad': False,
+        'betas': (0.9, 0.999)
+    },
+    'AdamAmsgrad':
+    {
+        'lr': tune.loguniform(1, 100),
+        'eps': 1e-8,
+        'amsgrad': True,
+        'betas': (0.9, 0.999)
+    }
+}
+
+SCHEDULERS_SEARCH_PBT = {
+    'CosineAnnealingLR':
+        {
+            'T_max': lambda steps: steps,
+            'eta_min': 0
+        },
+    'CosineAnnealingWarmRestarts':
+        {
+            'T_0': lambda steps: steps//2,
+            'T_mult': 1,
+            'eta_min': 0
+        },
+    'MultiStepLR':
+        {
+            'milestones': lambda steps: tune.grid_search(
+                [np.linspace(0, steps, 10),
+                 np.linspace(0, steps, 5),
+                 np.linspace(0, steps, 3)]
+            ),
+            'gamma': tune.uniform(0.1, 0.9)
+        },
+    'ReduceLROnPlateau':
+        {
+            'factor': tune.uniform(0.1, 0.5),
+            'patience': tune.choice([5, 10, 20]),
+            'threshold': 1e-5
+        }
+}
+
---
+++ tune_pbt.py
@@ -0,0 +1,179 @@
+import os
+import math
+import argparse
+import pickle
+from datetime import datetime
+
+import torch
+from ray import air
+from ray.air import session
+from ray import tune
+from ray.tune.search.flaml import CFO
+from ray.tune.schedulers import ASHAScheduler, PopulationBasedTraining
+
+from Attacks.FMN.FMNOptTune import FMNOptTune
+from Models.load_data import load_data
+from Configs.tuning_resources import TUNING_RES
+# from Configs.search_spaces_tune import OPTIMIZERS_SEARCH_TUNE, SCHEDULERS_SEARCH_TUNE
+from Configs.search_spaces_pbt import OPTIMIZERS_SEARCH_PBT, SCHEDULERS_SEARCH_PBT
+
+parser = argparse.ArgumentParser(description='Retrieve tuning params')
+parser.add_argument('-opt', '--optimizer',
+                    default='SGD',
+                    help='Provide the optimizer name (e.g. SGD, Adam)')
+parser.add_argument('-sch', '--scheduler',
+                    default='CosineAnnealingLR',
+                    help='Provide the scheduler name (e.g. CosineAnnealingLR)')
+parser.add_argument('-m_id', '--model_id',
+                    default=0,
+                    help='Provide the model ID')
+parser.add_argument('-d_id', '--dataset_id',
+                    default=0,
+                    help='Provide the dataset ID (e.g. CIFAR10: 0)')
+parser.add_argument('-b', '--batch',
+                    default=50,
+                    type=int,
+                    help='Provide the batch size (e.g. 50, 100, 10000)')
+parser.add_argument('-s', '--steps',
+                    default=10,
+                    type=int,
+                    help='Provide the steps number (e.g. 50, 100, 10000)')
+parser.add_argument('-n', '--norm',
+                    default='inf',
+                    help='Provide the norm (0, 1, 2, inf)')
+parser.add_argument('-n_s', '--num_samples',
+                    default=5,
+                    help='Provide the number of samples for the tuning')
+parser.add_argument('-ep', '--epochs',
+                    default=5,
+                    help='Provide the number of epochs for the attack tuning \
+                    (how many times the attack is run by the tuner)')
+parser.add_argument('-dp', '--dataset_percent',
+                    default=0.5,
+                    help='Provide the percentage of test dataset to be used to tune the hyperparams (default: 0.5)')
+parser.add_argument('-wp', '--working_path',
+                    default='TuningExp')
+
+args = parser.parse_args()
+
+
+def tune_attack(config, model, samples, labels, attack_params, epochs=5):
+    for epoch in range(epochs):
+        attack = FMNOptTune(
+            model=model,
+            inputs=samples.clone(),
+            labels=labels.clone(),
+            norm=attack_params['norm'],
+            steps=attack_params['steps'],
+            optimizer=attack_params['optimizer'],
+            scheduler=attack_params['scheduler'],
+            optimizer_config=config['opt_s'],
+            scheduler_config=config['sch_s']
+        )
+
+        distance, _ = attack.run()
+        session.report({"distance": distance})
+
+
+if __name__ == '__main__':
+    optimizer = args.optimizer
+    scheduler = args.scheduler
+    model_id = int(args.model_id)
+    dataset_id = int(args.dataset_id)
+    dataset_percent = float(args.dataset_percent)
+    working_path = args.working_path
+
+    # load search spaces
+    optimizer_search = OPTIMIZERS_SEARCH_PBT[optimizer]
+    scheduler_search = SCHEDULERS_SEARCH_PBT[scheduler]
+
+    attack_params = {
+        'batch': int(args.batch),
+        'steps': int(args.steps),
+        'norm': int(args.norm) if not isinstance(args.norm, str) else args.norm,
+        'optimizer': optimizer,
+        'scheduler': scheduler
+    }
+
+    tune_config = {
+        'num_samples': int(args.num_samples),
+        'epochs': int(args.epochs)
+    }
+
+    # load model and dataset
+    model, dataset = load_data(model_id, dataset_id, attack_params['norm'])
+
+    dataset_frac = list(range(0, math.floor(len(dataset)*dataset_percent)))
+    dataset_frac = torch.utils.data.Subset(dataset, dataset_frac)
+
+    dl_test = torch.utils.data.DataLoader(dataset_frac,
+                                          batch_size=attack_params['batch'],
+                                          shuffle=False)
+    samples, labels = next(iter(dl_test))
+
+    steps_keys = ['T_max', 'T_0', 'milestones']
+    for key in steps_keys:
+        if key in scheduler_search:
+            scheduler_search[key] = scheduler_search[key](attack_params['steps'])
+    search_space = {
+        'opt_s': optimizer_search,
+        'sch_s': scheduler_search
+    }
+
+    trainable_with_resources = tune.with_resources(
+        tune.with_parameters(
+            tune_attack,
+            model=model,
+            samples=samples,
+            labels=labels,
+            attack_params=attack_params,
+            epochs=tune_config['epochs']
+        ),
+        resources=TUNING_RES
+    )
+
+    # tune_scheduler = ASHAScheduler(mode='min', metric='distance', grace_period=2)
+    tune_scheduler = PopulationBasedTraining(
+        time_attr='training_iteration',
+        perturbation_interval=1,
+        metric='distance',
+        mode='min',
+        hyperparam_mutations=optimizer_search
+    )
+    # algo = CFO(metric='distance', mode='min')
+
+    # ./TuningExp/Modelname_dataset/...
+    time = datetime.now().strftime("%d%H%M")
+    tuning_exp_name = f"{optimizer}_{scheduler}_{time}"
+    tuning_exp_path = os.path.join(working_path, tuning_exp_name)
+    tuner = tune.Tuner(
+        trainable_with_resources,
+        param_space=search_space,
+        tune_config=tune.TuneConfig(
+            num_samples=tune_config['num_samples'],
+            # search_alg=algo,
+            scheduler=tune_scheduler
+        ),
+        run_config=air.RunConfig(tuning_exp_name, local_dir=working_path)
+    )
+
+    results = tuner.fit()
+
+    # Checking best result and best config
+    best_result = results.get_best_result(metric='distance', mode='min')
+    best_config = best_result.config
+
+    if 'distance' not in best_result.metrics:
+        exit(1)
+
+    print(f"best_distance : {best_result.metrics['distance']}\n, best config : {best_config}\n")
+
+    best_result_packed = {
+        'distance': best_result.metrics['distance'],
+        'best_config': best_result.config
+    }
+
+    filename = os.path.join(tuning_exp_path, "best_result.pkl")
+    with open(filename, "wb") as file:
+        pickle.dump(best_result_packed, file)
+
