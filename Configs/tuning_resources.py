2bca06fb9547e1c29293e28a11864ff9cdcd1a17
lucascionis <lucascionis@tiscali.it>
2023-04-21 13:07:26 +0200

    Added reference comment

--- Configs/tuning_resources.py
+++ Configs/tuning_resources.py
@@ -1,6 +1,24 @@
+"""
+Define here your Ray resources - for hyperparams tuning
+
+e.g. cpu x trial:
+
+{
+    'cpu': n,
+    'cpu': n,
+    ...
+}
+
+or cpu and gpu x trial
+
+{
+    'gpu': ...,
+    'cpu': ...,
+    ...
+}
+"""
+
 TUNING_RES = {
-    'cpu': 3,
-    'cpu': 3,
-    'cpu':3,
-    'cpu':3
+    'cpu': 6,
+    'cpu': 6
 }
