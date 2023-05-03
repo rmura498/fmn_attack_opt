d639e25aed89d2d8506a2a9b1dd468a5c178cc7f
lucascionis <lucascionis@tiscali.it>
2023-05-03 10:25:23 +0200

    Minor change

--- run_best_configs.py
+++ run_best_configs.py
@@ -1,7 +1,5 @@
 import os, argparse
 
-
-
 parser = argparse.ArgumentParser(description='Retrieve tuning params')
 parser.add_argument('-b', '--batch',
                     default=32,
@@ -16,10 +14,6 @@ parser.add_argument('-dp', '--dataset_percent',
 args = parser.parse_args()
 
 
-
-
-
-
 if __name__ == '__main__':
     batch = str(args.batch)
     steps = str(args.steps)
@@ -27,6 +21,8 @@ if __name__ == '__main__':
 
     tuning_cmds = []
     filenames = os.listdir("./Configs/ModelsBestConfigs")
+    print(len(filenames))
+    exit(0)
     for filename in filenames:
         tuning_cmd = f'python run_attack.py --batch {batch} --steps {steps} --dataset_percent {dt_percent} --fmn_config ./Configs/ModelsBestConfigs/{filename}\n'
         tuning_cmds.append(tuning_cmd)
