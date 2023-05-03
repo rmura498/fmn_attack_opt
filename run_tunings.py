48a73f264620217a64b8bbb754a43ada3382a79a
giorgiopiras <giorgiopiras4@gmail.com>
2023-05-03 12:20:40 +0200

    minor changes

--- run_tunings.py
+++ run_tunings.py
@@ -13,13 +13,13 @@ SCHEDULERS = [
 
 parser = argparse.ArgumentParser(description='Retrieve tuning params')
 parser.add_argument('-b', '--batch',
-                    default=50,
+                    default=100,
                     help='Provide the batch size')
 parser.add_argument('-s', '--steps',
                     default=100,
                     help='Provide the step size')
 parser.add_argument('-n_s', '--num_samples',
-                    default=1,
+                    default=20,
                     help='Provide the number of trials to execute')
 parser.add_argument('-ep', '--epochs',
                     default=1,
@@ -37,14 +37,14 @@ if __name__ == '__main__':
     epochs = str(args.epochs)
     dt_percent = str(args.dataset_percent)
 
-    tuning_cmds = []
     for model_id in MODEL_DATASET:
+        tuning_cmds = []
         model = MODEL_DATASET[model_id]
         model_name = model['model_name']
         datasets = model['datasets']
 
         for dataset_id, dataset_name in enumerate(datasets):
-            tuning_exp_wp = os.path.join("TuningExp", f"{model_name}_{dataset_name}")
+            tuning_exp_wp = os.path.join("TuningExpV2", f"{model_name}_{dataset_name}")
 
             for opt in OPTIMIZERS:
                 for sch in SCHEDULERS:
@@ -52,6 +52,6 @@ if __name__ == '__main__':
                         tuning_cmd = f'python tune.py --model_id {model_id} --dataset_id {dataset_id} --optimizer {opt} --scheduler {sch} --batch {batch} --steps {steps} --num_samples {num_s} --epochs {epochs} --dataset_percent {dt_percent} --working_path {tuning_exp_wp} --loss {i}\n'
                         tuning_cmds.append(tuning_cmd)
 
-    with open("tuning_cmd.txt", 'w') as f:
-        f.writelines(tuning_cmds)
+        with open("tuning_cmd_model{0}.txt".format(model_id), 'w') as f:
+            f.writelines(tuning_cmds)
 
