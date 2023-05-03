83dac3b53adbe4214bd69df840f1c9c173da21f6
rmura48 <r.ciompo@gmail.com>
2023-05-03 09:22:38 +0200

    added run_configurations.sh

--- run_best_configs.py
+++ run_best_configs.py
@@ -4,10 +4,10 @@ import os, argparse
 
 parser = argparse.ArgumentParser(description='Retrieve tuning params')
 parser.add_argument('-b', '--batch',
-                    default=10,
+                    default=32,
                     help='Provide the batch size')
 parser.add_argument('-s', '--steps',
-                    default=50,
+                    default=200,
                     help='Provide the step size')
 parser.add_argument('-dp', '--dataset_percent',
                     default=0.5,
