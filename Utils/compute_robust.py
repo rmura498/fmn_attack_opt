404fe63d8959914031f6581eb1949911ba9eeb63
lucascionis <lucascionis@tiscali.it>
2023-05-04 20:26:50 +0200

    Minor changes

--- Utils/compute_robust.py
+++ Utils/compute_robust.py
@@ -14,10 +14,11 @@ def compute_robust(exp_distances, best_distance):
     robust_per_iter = []
     for distance in exp_distances:
         # checking, for each step, the epsilon tensor
-        distances = np.concatenate((distances, distance), axis=None)
+        distances = np.concatenate((distances, distance.numpy()), axis=None)
         robust_per_iter += [
-            (np.count_nonzero(dist > best_distance) / batch_size)
+            (np.count_nonzero(dist.numpy() > best_distance) / batch_size)
             for dist in distance
         ]
 
     return distances, robust_per_iter
+
